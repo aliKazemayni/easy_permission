bd856094ecebc90935558cf8284ea55fbf6f7007
aliKazemayni <ali.kazemayni79@gmail.com>
2023-08-23 12:50:03 +0330

    fix route

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,4 @@
 <?php
 Route::apiResource('role' , \App\Http\Controllers\RoleController::class);
-Route::apiResource('section' , \App\Http\Controllers\SectionPermissionsController::class);
-Route::apiResource('permission' , \App\Http\Controllers\PermissionsController::class);
+Route::apiResource('section' , \App\Http\Controllers\SectionController::class);
+Route::apiResource('permission' , \App\Http\Controllers\PermissionController::class);
