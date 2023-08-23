59c10fcd0e54d12f68e6daac8b8f9930a7f84144
aliKazemayni <ali.kazemayni79@gmail.com>
2023-08-23 12:35:35 +0330

    fix migration create user_role

--- database/migrations/2023_08_07_120235_create_permissions_tables.php
+++ database/migrations/2023_08_07_120235_create_permissions_tables.php
@@ -48,7 +48,7 @@ return new class extends Migration
         });
 
         Schema::create('user_role', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained('user')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
             $table->timestamps();
         });
